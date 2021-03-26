4c0e3ee251a2fa84b5eb14564eaedb2d368f9885
Erick Zarat <ezarat15@gmail.com>
2021-03-26 00:08:13 -0600

    add database connection and medicamentos DAO implemantation

--- src/main/java/me/astridcortez/hispatologialab/medicamentos/dao/IMedicamentosDao.java
+++ src/main/java/me/astridcortez/hispatologialab/medicamentos/dao/IMedicamentosDao.java
@@ -1,4 +1,13 @@
 package me.astridcortez.hispatologialab.medicamentos.dao;
 
+import me.astridcortez.hispatologialab.medicamentos.dto.Medicamento;
+
+import java.util.List;
+
 public interface IMedicamentosDao {
+    List<Medicamento> getMedicamentos();
+    Medicamento getMedicamento(int codMedicamento);
+    Medicamento guardarMedicamento(Medicamento medicamento);
+    Medicamento modificarMedicamento(Medicamento medicamento);
+    Boolean darDeBaja(int codMedicamento);
 }
---
+++ src/main/java/me/astridcortez/hispatologialab/medicamentos/dao/MedicamentosDaoImpl.java
@@ -0,0 +1,108 @@
+package me.astridcortez.hispatologialab.medicamentos.dao;
+
+import me.astridcortez.hispatologialab.core.DB;
+import me.astridcortez.hispatologialab.medicamentos.dto.Medicamento;
+import me.astridcortez.hispatologialab.tables.LabMedicamento;
+import me.astridcortez.hispatologialab.tables.records.LabMedicamentoRecord;
+import org.jooq.DSLContext;
+import org.jooq.Record;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MedicamentosDaoImpl implements IMedicamentosDao {
+
+    private final DSLContext query = DB.getConexion();
+    private final LabMedicamento tabla = LabMedicamento.LAB_MEDICAMENTO;
+
+
+    public Medicamento parseItem(Record record) {
+        return new Medicamento (
+                record.getValue(tabla.COD_MEDICAMENTO),
+                record.getValue(tabla.NOMBRE_MEDICAMENTO),
+                record.getValue(tabla.ESTADO_MEDICAMENTO),
+                record.getValue(tabla.CREADO_POR),
+                record.getValue(tabla.FECHA_CREACION),
+                record.getValue(tabla.MODIFICADO_POR),
+                record.getValue(tabla.FECHA_MODIFICACION)
+        );
+    }
+
+    @Override
+    public List<Medicamento> getMedicamentos() {
+        List<Record> results = query
+                .select(tabla.asterisk())
+                .from(tabla)
+                .fetch();
+        return results.stream().map(this::parseItem).collect(Collectors.toList());
+    }
+
+    public List<Medicamento> getMedicamentos(int limit) {
+        List<Record> results = query
+                .select(tabla.asterisk())
+                .from(tabla)
+                .orderBy(tabla.COD_MEDICAMENTO)
+                .limit(limit)
+                .fetch();
+        return results.stream().map(this::parseItem).collect(Collectors.toList());
+    }
+
+    public List<Medicamento> getMedicamentos(int lastId, int limit) {
+        List<Record> results = query
+                .select(tabla.asterisk())
+                .from(tabla)
+                .orderBy(tabla.COD_MEDICAMENTO)
+                .seek(lastId)
+                .limit(limit)
+                .fetch();
+        return results.stream().map(this::parseItem).collect(Collectors.toList());
+    }
+
+
+    public List<Medicamento> getMedicamentosPaginated() {
+        List<Record> results = query
+                .select(tabla.asterisk())
+                .from(tabla)
+                .fetch();
+        return results.stream().map(this::parseItem).collect(Collectors.toList());
+    }
+
+    @Override
+    public Medicamento getMedicamento(int codMedicamento) {
+        Record result = query
+                .select(tabla.asterisk())
+                .from(tabla)
+                .where(tabla.COD_MEDICAMENTO.eq(codMedicamento))
+                .fetchOne();
+        return result != null ? parseItem(result): null;
+    }
+
+    @Override
+    public Medicamento guardarMedicamento(Medicamento medicamento) {
+        LabMedicamentoRecord record =  query.newRecord(tabla);
+        record.setNombreMedicamento(medicamento.getNombreMedicamento());
+        record.setFechaCreacion(LocalDate.now());
+        record.setCreadoPor("");
+        record.setEstadoMedicamento("");
+        int id = record.store();
+        return getMedicamento(id);
+    }
+
+    @Override
+    public Medicamento modificarMedicamento(Medicamento medicamento) {
+        int id = query.update(tabla)
+                .set(tabla.NOMBRE_MEDICAMENTO, medicamento.getNombreMedicamento())
+                .set(tabla.FECHA_MODIFICACION, LocalDate.now())
+                .set(tabla.MODIFICADO_POR, "")
+                .set(tabla.ESTADO_MEDICAMENTO, "")
+                .where(tabla.COD_MEDICAMENTO.eq(medicamento.getCodigoMedicamento()))
+                .execute();
+        return getMedicamento(id);
+    }
+
+    @Override
+    public Boolean darDeBaja(int codMedicamento) {
+        return null;
+    }
+}
