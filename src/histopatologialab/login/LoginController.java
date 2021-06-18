84231a47d7953e0312bf20de7f8062e472c19669
Erick Zarat <ezarat15@gmail.com>
2021-06-17 19:27:50 -0600

    add role management

--- src/histopatologialab/login/LoginController.java
+++ src/histopatologialab/login/LoginController.java
@@ -8,7 +8,14 @@ import histopatologialab.password.PasswordUtils;
 import histopatologialab.usuario.dto.Usuario;
 import histopatologialab.usuario.controller.IUsuarioController;
 
+import histopatologialab.core.Role;
+import histopatologialab.core.RoleHandler;
+
+import javax.servlet.http.HttpSession;
+
 public class LoginController {
+
+    private final String ROLE_KEY = "user_role";
 
     public Boolean iniciarSession(String usuario, String password) {
 
@@ -20,6 +27,9 @@ public class LoginController {
 	        	 boolean passwordMatch = PasswordUtils.verifyUserPassword(password, usuarioBD.getPasswordUsuario());
 	            if(passwordMatch) 
 	            { System.out.println("El password ingresado es correcto.");
+
+                    Role role = Role.NORMAL; //TODO: cambiar esto por el rol de la base de datos
+                    crearSesion(session, usuario, role);
 		    		return true; 
 	            } else {
 	                System.out.println("El password ingresado no es correcto");
@@ -33,5 +43,17 @@ public class LoginController {
 		} catch (Exception e) {
 			return false;
 		}    	    	   	
+
+    }
+
+    public void crearSesion(HttpSession session, String usuario, Role role) {
+        session.setAttribute(ROLE_KEY, role.getSlug());
+        session.setAttribute("usuario", usuario);
+        session.setAttribute("sesionIniciada", true);
+    }
+
+    public void cerrarSesion(HttpSession session) {
+        if (session == null) return;
+        session.invalidate();
     }
 }
