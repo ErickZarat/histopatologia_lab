403e187309b9a24301d08f26ee97c77452e68943
AstridC <astridcortez@gmail.com>
2021-07-11 20:51:58 -0600

    modificaciones en cambio de psw y salir
    Agregar validaciones en js cambio de psw y salida

--- WebContent/assets/js/mantenimientos/login.js
+++ WebContent/assets/js/mantenimientos/login.js
@@ -12,67 +12,68 @@ $(document).ready(function() {
 	}
 
 
+
+	Messenger.options = {
+		extraClasses : 'messenger-fixed messenger-on-top',
+		theme: 'future'
+	};	
+
+	jQuery.validator.setDefaults({
+  		debug: true,
+  		success: "valid"
+	});
+	
+
   
     $('#btnCancelCerrarSesion').click(function(){
  		 $("#logoutUsuarioModal")[0].reset();
     });
 	
     $('#btnCancelCambioPsw').click(function(){
- 		 $("#cambioPswUsuarioModal")[0].reset();
+ 		$("#CambioPswFormModal")[0].reset();
+		$('#cambioPswUsuarioModal label.error').hide();
+		
     });
 	
+	$.validator.addMethod("loginFormato", function(value, element) {
+                return this.optional(element) || /^[a-zA-Z0-9\_\-]{7,20}$/i.test(value);
+	}, "El login ingresado es inválido.");	
 
-    $('#btnModificarUsuario').click(function(e){
-        var codigoUsuario = $('#codigoUsuarioMod').val();
-        var loginUser = $('#loginUsuarioMod').val();
-		var nombresDoctor =  $('#nombresUsuarioMod').val();
-		var apellidosDoctor = $('#apellidosUsuarioMod').val();
-		var emailDoctor = $('#emailUsuarioMod').val();
-		var colegiadoDoctor = $('#colegiadoUsuarioMod').val();
-		//var tipoUsuario = $('#tipUsuarioMod').val();
-		var tipoUsuario = $('#tipoUsuarioModSearch').val();
-
-
-		if ($('#nombresUsuarioMod').val().trim() == "" || $('#apellidosUsuarioMod').val().trim() == "") {
-			 toastr.error("Debe ingresar por lo menos un nombre y un apellido de usuario");
-			 e.preventDefault(); 
-		} else { 	
-			if(!(validarEmail(emailDoctor)||emailDoctor.trim()=='')){
-				toastr.error("El correo ingresado no es válido");
-				e.preventDefault();
-			} else {
-		        $.ajax({
-		            url: 'UsuarioServlet.do',
-		            method: 'post',
-		            data: {
-		                accion: 'MODIFICAR',
-		                loginUser: loginUser,
-						nombresDoctor: nombresDoctor,
-						apellidosDoctor: apellidosDoctor,
-						emailDoctor: emailDoctor,
-						colegiadoDoctor: colegiadoDoctor,
-						tipoUsuario: tipoUsuario,
-		            },
-		            success: function(response) {
-		                $('#modificarUsuarioModal').modal('hide');
-						$("#ModificarFormModal")[0].reset();
-		                if (response.success){
-		                    toastr.success("Se modifico con exito el Usuario");
-		                    getListadoUsuarios();
-		                } else {
-		                    toastr.error("Error al modificar el Usuario");
-		                }
-		            }
-		        });
-			}	
-		}
-	//e.preventDefault();
-    });
-
-		Messenger.options = {
-			extraClasses : 'messenger-fixed messenger-on-top',
-			theme : 'flat'
-		};	
+	var validar_formularioMod = $("#CambioPswFormModal").validate({
+ 	rules :{
+ 			passwordAnterior : { required : true, loginFormato :true,  minlength : 6, maxlength : 50 },							
+            nuevoPassword : { required : true, loginFormato :true,  minlength : 6, maxlength : 50 },
+			confirNuevoPassword : { required : true, loginFormato :true, equalTo: '#nuevoPassword', minlength : 6, maxlength : 50 },               
+            },
+            messages : {
+                nuevoPassword : {
+                    required : "Debe ingresar el nuevo Password",
+                    minlength : "El Password es incorrecto",
+                    maxlength : "El máximo deben ser 50 caracteres"
+                },
+                confirNuevoPassword : {
+                    required : "Debe ingresar la confirmación del password",
+					equalTo: "El password y la Confirmación no coinciden", 
+                    minlength : "El password es incorrecto"
+                },
+                passwordAnterior : {
+                    required : "Debe ingresar el Password anterior",
+                    minlength : "El Password ingresado es incorrecto"
+                },
+ 		errorElement: "em",
+       	errorPlacement: function (error, element) {
+          // Add the `help-block` class to the error element
+          error.addClass("help-block"); 
+       	},
+       	highlight: function ( element, errorClass, validClass ) {
+          $( element ).parents( ".col-sm-10" ).addClass( "has-error" ).removeClass( "has-success" );
+       	},
+       	unhighlight: function (element, errorClass, validClass) {
+          $( element ).parents( ".col-sm-10" ).addClass( "has-success" ).removeClass( "has-error" );  
+       	} 
+	}
+  	});		
+		
 
 
   $('#btnCambioPsw').click(function(e){
@@ -80,63 +81,71 @@ $(document).ready(function() {
 		var pswAnterior = $('#passwordAnterior').val();
 		var pswActual = $('#nuevoPassword').val();
 		
-	if ($('#passwordAnterior').val().trim().length == 0) {
-				toastr.error("Debe ingresar la contraseña actual");
-					e.preventDefault(); 
-			} else { 		
-				if ($('#nuevoPassword').val().trim().length == 0 || $('#confirNuevoPassword').val().trim().length == 0) {
-						toastr.error("Debe ingresar la nueva contraseña y la ConfirmaciÓ de Contraseña");
-						e.preventDefault(); 
-				} else { 	
-					if ($('#nuevoPassword').val().trim() != $('#confirNuevoPassword').val().trim()) {
-						toastr.error("La nueva contraseña y la Confirmación no coinciden");
-						e.preventDefault();		
-					} else {
-						var mensajito=Messenger().post({
-							message:"¿Está seguro que desea cambiar su contraseña?",
-						  	type:'info',
-						  	showCloseButton:true,
-						  	id:1,
-						  	hideAfter:36000,
-						  	actions: {
-						  		aceptar: {
-						 			label: "Aceptar",
-						   			action: function(){  //ejecutarComando();
-											mensajito.hide();		
-									        $.ajax({
-								            url: 'UsuarioServlet.do',
-								            method: 'post',
-								            data: {
-								                accion: 'CAMBIO_PSW',
-								                loginUser: loginUser,
-												pswAnterior: pswAnterior,
-												pswActual :pswActual,
-								            },
-								            success: function(response) {
-								                if (response.success){
-									                $('#cambioPswUsuarioModal').modal('hide');
-													$("#CambioPswFormModal")[0].reset();									
-								                    toastr.success("Se cambio con éxito la contraseña");
-								                } else {
-								                    toastr.error(response.error);
-								                }
-								            }
-								        });	
-						
-										}
-						    	},
-						   		cancelar: {
-							 		label: "Cancelar",
-							   		action: function(){mensajito.hide();}
-									}
-						  		}
-							});		// mensajito
-					}				 	
-				}	 	
-			} // else de condiciones 
-		e.preventDefault();
+	 	if (validar_formularioMod.form()) //asi se comprueba si el form esta validado o no
+    	{ 	
+			var mensajito=Messenger().post({
+				message:"¿Está seguro que desea cambiar su contraseña?",
+			  	type:'info',
+			  	showCloseButton:true,
+			  	id:1,
+			  	hideAfter:36000,
+			  	actions: {
+			  		aceptar: {
+			 			label: "Aceptar",
+			   			action: function(){  //ejecutarComando();
+								mensajito.hide();		
+						        $.ajax({
+					            url: 'UsuarioServlet.do',
+					            method: 'post',
+					            data: {
+					                accion: 'CAMBIO_PSW',
+					                loginUser: loginUser,
+									pswAnterior: pswAnterior,
+									pswActual :pswActual,
+					            },
+					            success: function(response) {
+					                if (response.success){
+						                $('#cambioPswUsuarioModal').modal('hide');
+										$("#CambioPswFormModal")[0].reset();									
+					                    toastr.success("Se cambio con éxito la contraseña");
+					                } else {
+					                    toastr.error(response.error);
+					                }
+					            }
+					        });	
+			
+							}
+			    	},
+			   		cancelar: {
+				 		label: "Cancelar",
+				   		action: function(){mensajito.hide();}
+						}
+			  		}
+				});		// mensajito
+		} else {
+			e.preventDefault();
+		}
     });
 
 
-
+ $('#btnCerrarSesion').click(function(e){
+        var loginUser = $('#loginUsuario').val();	
+		        $.ajax({
+	            url: 'UsuarioServlet.do',
+	            method: 'post',
+	            data: {
+	                accion: 'CONFIRM_SALIR',
+	                loginUser: loginUser
+	            },
+	            success: function(response) {
+	                if (response.success){
+		                $('#cambioPswUsuarioModal').modal('hide');
+						$("#CambioPswFormModal")[0].reset();									
+	                    toastr.success("Se cambio con éxito la contraseña");
+	                } else {
+	                    toastr.error(response.error);
+	                }
+	            }
+	        });	
+    });
 });
